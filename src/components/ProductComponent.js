b6079fa00a5256730943c65f3370bc2b23eebbcc
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2021-09-24 19:17:58 -0300

    test

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -5,16 +5,13 @@ import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
   const gallery = useSelector((state) => state.allProducts.products);
-  let images = {};
+  let images = "";
   let description = "";
   const [img, setImg] = useState(images);
   const [des, setDes] = useState(description);
 
   const renderList = gallery.map((product) => {
     const { id, title, images } = product;
-    setImg(images);
-    setDes(title);
-
     setTimeout(() => {
       setImg(images[0].link);
       setDes(images[0].description);
@@ -27,11 +24,7 @@ const ProductComponent = () => {
           <div className="ui link cards">
             <div className="card">
               <div className="image">
-                {img ? (
-                  <img src={img} alt={title} />
-                ) : (
-                  <img src={images} alt={title} />
-                )}
+                <img src={img} alt={title} />
               </div>
               <div className="content">
                 <div className="header">{des}</div>
