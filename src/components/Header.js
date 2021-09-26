2d78382917528d00a455eaecd32063ba4181eaec
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2021-09-26 19:24:54 -0300

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IoArrowBackCircleOutline } from "react-icons";
 
 const Header = () => {
   return (
     <div className="ui fixed menu">
       <div className="ui container center">
         <Link to="/">
-          <h2 style={{ cursor: "pointer", color: "black" }}>Challenge</h2>
+          <h2 style={{ cursor: "pointer", color: "black" }}>
+            {" "}
+            <IoArrowBackCircleOutline />
+          </h2>
         </Link>
       </div>
     </div>
